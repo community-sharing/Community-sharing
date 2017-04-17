383d890b71c88f238292b394cd126f962aea6d81
todddrinkwater <todddrinkwaternz@gmail.com>
2017-04-17 13:04:31 +1200

    fix share card to render item_name not description

--- client/components/SharedCard.jsx
+++ client/components/SharedCard.jsx
@@ -3,13 +3,14 @@ import { Link } from 'react-router-dom'
 import { fetchSingleItem } from '../actions'
 
 function SharedCard (props) {
+  console.log(props)
   return (
     <div className='card-wrapper'>
       <div className='card-container'>
         <img className='card-image' src={props.loggedInImage}></img>
 
         <p className='shared-card-details'>
-          <span className='shared-name'>You</span> are sharing your {props.description} with <span className='shared-name'>{props.fname}</span>
+          <span className='shared-name'>You</span> are sharing your {props.item_name} with <span className='shared-name'>{props.fname}</span>
         </p>
 
         <img className='card-image' src='http://vignette1.wikia.nocookie.net/sote-rp/images/c/c4/User-placeholder.png/revision/latest?cb=20150624004222'></img>
